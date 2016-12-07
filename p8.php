b01c48288e53e4264bf4f6990786afdcca9ffad3
qwert12345asdfg <1032437852@qq.com>
2016-12-07 11:44:44 +0800

    p8 修改
    第一次修改

--- p8.php
+++ p8.php
@@ -1,8 +1,8 @@
 <?php 
 echo "<br/>数值强制转换："; 
-$string="2a"; 
+$string="2a大点的过过过过过过"; 
 $string1=intval($string); 
 echo '$string1的值：'.$string1.'$string2的值：';//单引号不会输出变量，将原样输出 
 $string2=(int)($string); 
-echo $string2 ;
+echo $string2 ;echo $string2 ;echo $string2 ;echo $string2 ;echo $string2 ;
 ?>
